06ec911e1299936986cf3527b97574536cc570f6
FeuFeve <clement-potin@outlook.com>
2024-06-24 14:04:39 +0200

    Create vshader and fshader

--- Program.cs
+++ Program.cs
@@ -1,3 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using OpenTK.Graphics.ES20;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
 
-Console.WriteLine("Hello, Voxen!");
+int p = -1;
+
+GameWindowSettings gameWindowSettings = new GameWindowSettings()
+{
+    UpdateFrequency = 60.0f,
+};
+
+NativeWindowSettings navNativeWindowSettings = new NativeWindowSettings()
+{
+    Title = "Voxen",
+    WindowState = WindowState.Maximized
+};
+
+GameWindow gameWindow = new(gameWindowSettings, navNativeWindowSettings);
+
+gameWindow.Load += () =>
+{
+    Console.WriteLine("Loaded");
+    
+    p = CreateShaders();
+    GL.UseProgram(p);
+};
+
+gameWindow.Unload += () =>
+{
+    GL.DeleteProgram(p);
+};
+
+gameWindow.Run();
+
+int CreateShaders()
+{
+    int vShader = GL.CreateShader(ShaderType.VertexShader);
+    int fShader = GL.CreateShader(ShaderType.FragmentShader);
+    
+    GL.ShaderSource(vShader, File.ReadAllText("../../../Shaders/vshader.glsl"));
+    GL.ShaderSource(fShader, File.ReadAllText("../../../Shaders/fshader.glsl"));
+    
+    GL.CompileShader(vShader);
+    GL.CompileShader(fShader);
+    
+    Console.WriteLine(GL.GetShaderInfoLog(vShader));
+    Console.WriteLine(GL.GetShaderInfoLog(fShader));
+
+    int program = GL.CreateProgram();
+    GL.AttachShader(program, vShader);
+    GL.AttachShader(program, fShader);
+    GL.DetachShader(program, vShader);
+    GL.DetachShader(program, fShader);
+    GL.DeleteShader(vShader);
+    GL.DeleteShader(fShader);
+    
+    Console.WriteLine(GL.GetProgramInfoLog(program));
+    
+    return program;
+}
---
+++ Shaders/vshader.glsl
@@ -0,0 +1,13 @@
+#version 460
+
+layout(location=0) in vec3 vertices;
+layout(location=1) in vec3 colors;
+
+out vec3 fragColor;
+
+void main()
+{
+    fragColor = colors;
+
+    gl_Position = vec4(vertices, 1.0f);
+}
