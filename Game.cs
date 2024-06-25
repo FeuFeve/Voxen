f871ddb0dab8ac7b461d4402816f660369620405
FeuFeve <clement-potin@outlook.com>
2024-06-25 14:00:09 +0200

    Clear color, handle resizing, handle fullscreen/normal mode with F11

--- Game.cs
+++ Game.cs
@@ -1,3 +1,4 @@
+using OpenTK.Graphics.ES20;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
@@ -19,11 +20,36 @@ public class Game : GameWindow
 
     #region GameWindow overriden methods
 
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+        
+        GL.ClearColor(0.2f, 0.2f, 0.3f, 1.0f);
+    }
+
     protected override void OnUpdateFrame(FrameEventArgs args)
     {
         base.OnUpdateFrame(args);
 
         HandleKeyboardEvents();
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        base.OnRenderFrame(args);
+        
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+        
+        
+        
+        SwapBuffers();
+    }
+
+    protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
+    {
+        base.OnFramebufferResize(e);
+        
+        GL.Viewport(0, 0, e.Width, e.Width);
     }
 
     #endregion
@@ -40,6 +66,12 @@ public class Game : GameWindow
         if (KeyboardState.IsKeyDown(Keys.Escape))
         {
             Close();
+        }
+        else if (KeyboardState.IsKeyPressed(Keys.F11))
+        {
+            WindowState = WindowState == WindowState.Fullscreen
+                ? WindowState.Normal
+                : WindowState.Fullscreen;
         }
     }
 
