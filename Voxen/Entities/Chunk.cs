2a3a7ec689f27493660da3ca3fb5148a3649514b
FeuFeve <clement-potin@outlook.com>
2024-07-02 14:08:45 +0200

    Create IEntity

---
+++ Voxen/Entities/Chunk.cs
@@ -0,0 +1,36 @@
+﻿using OpenTK.Mathematics;
+using Voxen.World;
+
+namespace Voxen.Entities;
+
+public struct Chunk : IEntity
+{
+    #region Constructor
+
+    public Chunk()
+    {
+        Guid = Guid.NewGuid();
+    }
+
+    #endregion
+
+    #region Properties
+
+    public Guid Guid { get; }
+
+    public Vector3i ChunkCoordinates { get; set; }
+    
+    public VoxelType[] Voxels { get; set; } = [];
+
+    #endregion
+    
+    #region Constants
+
+    // The chunks are CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE voxels
+    public const ushort CHUNK_SIZE = 16;
+
+    // The voxels are VOXEL_SIZE meters * VOXEL_SIZE meters * VOXEL_SIZE meters
+    public const float VOXEL_SIZE = 1.0f;
+
+    #endregion
+}
---
+++ Voxen/Entities/IEntity.cs
@@ -0,0 +1,10 @@
+namespace Voxen.Entities;
+
+public interface IEntity
+{
+    #region Properties
+
+    public Guid Guid { get; }
+
+    #endregion
+}
